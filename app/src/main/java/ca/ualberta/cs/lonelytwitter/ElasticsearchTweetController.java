f525e1a712a3832bb80ccf56cd2b3773ed37f0d9
michelleTagarino <mctagarino@gmail.com>
2016-10-20 18:32:34 -0600

    lab7 elastic search

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/ElasticsearchTweetController.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/ElasticsearchTweetController.java
@@ -7,38 +7,89 @@ import com.searchly.jestdroid.DroidClientConfig;
 import com.searchly.jestdroid.JestClientFactory;
 import com.searchly.jestdroid.JestDroidClient;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+import io.searchbox.client.JestClient;
+import io.searchbox.core.DocumentResult;
 import io.searchbox.core.Index;
+import io.searchbox.core.Search;
+import io.searchbox.core.SearchResult;
 
 /**
- * Created by romansky on 10/20/16.
+ * Created by romansky on 10/18/16.
  */
 public class ElasticsearchTweetController {
     private static JestDroidClient client;
 
-    // TODO we need a function which adds tweets to elastic search
+    // TODO we need a function that gets tweets!
+    public static class GetTweetsTask extends AsyncTask<String, Void, ArrayList<NormalTweet>> {
+        @Override
+        protected ArrayList<NormalTweet> doInBackground(String... search_parameters) {
+            verifySettings();
+
+            ArrayList<NormalTweet> tweets = new ArrayList<NormalTweet>();
+
+            String search_string = "{\"from\": 0, \ "size\": 10000";
+
+            // assume that search_parameters[0] is the only search term we are interested in using
+            Search search = new Search.Builder(search_parameters[0])
+                    .addIndex("testing")
+                    .addType("tweet")
+                    .build();
+
+            try {
+                SearchResult result = client.execute(search);
+                if (result.isSucceeded()) {
+                    List<NormalTweet> foundTweets = result.getSourceAsObjectList(NormalTweet.class);
+                    tweets.addAll(foundTweets);
+                }
+                else {
+                    Log.i("Error", "The search query failed to find any tweets that matched.");
+                }
+            }
+            catch (Exception e) {
+                Log.i("Error", "Something went wrong when we tried to communicate with the elasticsearch server!");
+            }
+
+            return tweets;
+        }
+    }
+
+
+    // TODO we need a function which adds a tweet!
     public static class AddTweetsTask extends AsyncTask<NormalTweet, Void, Void> {
 
         @Override
         protected Void doInBackground(NormalTweet... tweets) {
+            verifySettings();
 
-            for (NormalTweet tweet : tweets) {
+            for (NormalTweet tweet: tweets) {
                 Index index = new Index.Builder(tweet).index("testing").type("tweet").build();
 
                 try {
-                    // where is the client?
+                    DocumentResult result = client.execute(index);
+                    if (result.isSucceeded()) {
+                        tweet.setId(result.getId());
+                    }
+                    else {
+                        Log.i("Error", "Elastic search was not able to add the tweet.");
+                    }
                 }
                 catch (Exception e) {
-                    Log.i("Error", "The application failed to build and send the tweets");
+                    Log.i("Uhoh", "We failed to add a tweet to elastic search!");
+                    e.printStackTrace();
                 }
-
             }
+
             return null;
         }
     }
 
-    // TODO we need a function which gets tweets from elastic search
 
-    public static void verifySettings() {
+    private static void verifySettings() {
+        // if the client hasn't been initialized then we should make it!
         if (client == null) {
             DroidClientConfig.Builder builder = new DroidClientConfig.Builder("http://cmput301.softwareprocess.es:8080");
             DroidClientConfig config = builder.build();
@@ -48,4 +99,5 @@ public class ElasticsearchTweetController {
             client = (JestDroidClient) factory.getObject();
         }
     }
+
 }
